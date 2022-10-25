3b049f15a3f2f02498ada45997e5b651f19329ff
Shavon <95557107+snand5@users.noreply.github.com>
2022-10-25 21:14:39 +1000

    JS Tutorial

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,24 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+  
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
+  
