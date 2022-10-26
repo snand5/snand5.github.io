be49285459daffa0f632d53a0b8e4a4a7b371a41
Shavon <95557107+snand5@users.noreply.github.com>
2022-10-26 21:03:49 +1000

    add dark mode

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,4 @@
-
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-function setUserName() {
-    const myName = prompt("Please enter your name.");
-    if (!myName) {
-      setUserName();
-    } else {
-      localStorage.setItem("name", myName);
-      myHeading.textContent = `Mozilla is cool, ${myName}`;
-    }
-  }
-  
-  if (!localStorage.getItem("name")) {
-    setUserName();
-  } else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Mozilla is cool, ${storedName}`;
-  }
-  myButton.onclick = () => {
-    setUserName();
-  };
-  
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  } 
